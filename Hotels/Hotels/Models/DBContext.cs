2d9d3381399a8572141eee960b91c15258c1bd9b
AnaTorres <aitorresp@unal.edu.co>
2014-09-13 14:06:00 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Hotels/Hotels/Models/Reserva.cs
    	Hotels/Hotels/Views/Home/Index.cshtml

---
+++ Hotels/Hotels/Models/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace Hotels.Models
+{
+    public class DBContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, add the following
+        // code to the Application_Start method in your Global.asax file.
+        // Note: this will destroy and re-create your database with every model change.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<Hotels.Models.DBContext>());
+
+        public DBContext() : base("name=DBContext")
+        {
+        }
+
+        public DbSet<Reserva> Reservas { get; set; }
+
+        public DbSet<Habitacion> Habitacions { get; set; }
+    }
+}
--- Hotels/Hotels/Models/Habitacion.cs
+++ Hotels/Hotels/Models/Habitacion.cs
@@ -3,22 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hotels.Models
 {
     public class Habitacion
     {
+        [Key]
         public int ID { get; set; }
         public bool Avalible { get; set; }
         public DateTime StarDate { get; set; }
         public DateTime EndDate { get; set; }
         public decimal Price { get; set; }
+        public virtual ICollection<Reserva> Reserva { get; set; }
 
-        public ICollection<Reserva> Reserva { get; set; }
     }
 
-    public class HabitacionDBContext : DbContext
-    {
-        public DbSet<Habitacion> Habitaciones { get; set; }
-    }
 }
