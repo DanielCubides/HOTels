84471ffc3531f221ed9c7897db632d018ee12b3d
Jicaor <jiacardenasor@unal.edu.co>
2014-09-13 15:59:22 -0500

    Mejoras en la interfaz
    Se cambia algunas palabras al español

--- Hotels/Hotels/Models/AccountModels.cs
+++ Hotels/Hotels/Models/AccountModels.cs
@@ -42,18 +42,18 @@ namespace Hotels.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Contraseña actual")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "La contraseña debe se de al menos {2} caracteres", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nueva contraseña")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Confirmar nueva contraseña")]
+        [Compare("NewPassword", ErrorMessage = "La nueva contraseña y la confirmación de contraseña no coinciden.")]
         public string ConfirmPassword { get; set; }
     }
 
