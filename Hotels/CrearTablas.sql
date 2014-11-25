4ac2a24769b21b367385559a95b24cbeda98d2cf
DanielCubides <niroxdf@gmail.com>
2014-11-25 05:56:10 -0500

    Proyecto Funcionando

---
+++ Hotels/CrearTablas.sql
@@ -0,0 +1,73 @@
+﻿CREATE TABLE [dbo].[UserProfile] (
+    [UserId] INT            IDENTITY (1, 1) NOT NULL,
+    [Email]  NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
+CREATE TABLE [dbo].[Habitacions] (
+    [ID]       INT             IDENTITY (1, 1) NOT NULL,
+    [Avalible] BIT             NOT NULL,
+    [StarDate] DATETIME        NOT NULL,
+    [EndDate]  DATETIME        NOT NULL,
+    [Price]    DECIMAL (18, 2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+CREATE TABLE [dbo].[Reservas] (
+    [ID]             INT      IDENTITY (1, 1) NOT NULL,
+    [StartDate]      DATETIME NOT NULL,
+    [EndDate]        DATETIME NOT NULL,
+    [HabitacionID]   INT      NOT NULL,
+    [UsuarioID]      INT      NOT NULL,
+    [usuario_UserId] INT      NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [Habitacion_Reserva] FOREIGN KEY ([HabitacionID]) REFERENCES [dbo].[Habitacions] ([ID]) ON DELETE CASCADE,
+    CONSTRAINT [UserProfile_Reserva] FOREIGN KEY ([usuario_UserId]) REFERENCES [dbo].[UserProfile] ([UserId])
+);
+
+
+
+
+
+CREATE TABLE [dbo].[webpages_Membership] (
+    [UserId]                                  INT            NOT NULL,
+    [CreateDate]                              DATETIME       NULL,
+    [ConfirmationToken]                       NVARCHAR (128) NULL,
+    [IsConfirmed]                             BIT            DEFAULT ((0)) NULL,
+    [LastPasswordFailureDate]                 DATETIME       NULL,
+    [PasswordFailuresSinceLastSuccess]        INT            DEFAULT ((0)) NOT NULL,
+    [Password]                                NVARCHAR (128) NOT NULL,
+    [PasswordChangedDate]                     DATETIME       NULL,
+    [PasswordSalt]                            NVARCHAR (128) NOT NULL,
+    [PasswordVerificationToken]               NVARCHAR (128) NULL,
+    [PasswordVerificationTokenExpirationDate] DATETIME       NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
+
+
+CREATE TABLE [dbo].[webpages_OAuthMembership] (
+    [Provider]       NVARCHAR (30)  NOT NULL,
+    [ProviderUserId] NVARCHAR (100) NOT NULL,
+    [UserId]         INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Provider] ASC, [ProviderUserId] ASC)
+);
+
+
+
+CREATE TABLE [dbo].[webpages_Roles] (
+    [RoleId]   INT            IDENTITY (1, 1) NOT NULL,
+    [RoleName] NVARCHAR (256) NOT NULL,
+    PRIMARY KEY CLUSTERED ([RoleId] ASC),
+    UNIQUE NONCLUSTERED ([RoleName] ASC)
+);
+
+CREATE TABLE [dbo].[webpages_UsersInRoles] (
+    [UserId] INT NOT NULL,
+    [RoleId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC, [RoleId] ASC),
+    CONSTRAINT [fk_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[UserProfile] ([UserId]),
+    CONSTRAINT [fk_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[webpages_Roles] ([RoleId])
+);
+
+
--- Hotels/Hotels/Filters/InitializeSimpleMembershipAttribute.cs
+++ Hotels/Hotels/Filters/InitializeSimpleMembershipAttribute.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Hotels.Models;
+using System;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Threading;
 using System.Web.Mvc;
 using WebMatrix.WebData;
-using Hotels.Models;
 
 namespace Hotels.Filters
 {
